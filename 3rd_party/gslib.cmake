e812e219b7d7628affaa51435d93ef08fcab5854
Thilina Ratnayaka <thilinarmtb@gmail.com>
2023-09-11 17:09:13 -0500

    Build and link gslib

---
+++ 3rd_party/gslib.cmake
@@ -0,0 +1,18 @@
+include(ExternalProject)
+
+set(GSLIB_SOURCE_DIR ${CMAKE_BINARY_DIR}/gslib)
+set(GSLIB_INSTALL_DIR ${GSLIB_SOURCE_DIR}/build)
+
+ExternalProject_Add(gslib
+  GIT_REPOSITORY    https://github.com/Nek5000/gslib.git
+  GIT_TAG           v1.0.8
+  SOURCE_DIR        ${GSLIB_SOURCE_DIR}
+  INSTALL_DIR       ${GSLIB_INSTALL_DIR}
+  BUILD_IN_SOURCE   TRUE
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND     CC=${CMAKE_C_COMPILER} LIBNAME=gs CFLAGS=${CMAKE_C_FLAGS} make -C <SOURCE_DIR>
+)
+
+add_dependencies(parilu gslib)
+target_link_libraries(parilu ${GSLIB_INSTALL_DIR}/lib/libgs.a)
+target_include_directories(parilu PRIVATE ${GSLIB_INSTALL_DIR}/include)
