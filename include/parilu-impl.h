278f76b292699c3b9786aae96087c926b6fe64b9
Thilina Ratnayaka <thilinarmtb@gmail.com>
2023-10-11 16:33:29 -0500

    Get rid of verbose field from parilu_options

--- include/parilu-impl.h
+++ include/parilu-impl.h
@@ -43,7 +43,6 @@
  * Struct to hold the options for the ILU preconditioner.
  */
 struct parilu_options_t {
-  unsigned verbose;    /**< Verbosity level: 0, 1, 2, ... */
   unsigned type;       /**< ILU type: ILU(0), ILUC, etc. */
   unsigned pivot;      /**< Use pivoting or not. */
   unsigned null_space; /**< Is there a null space? */
